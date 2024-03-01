badeedfb3a3d88e1abd9ab9ba209134fd84d0c78
Extra-Creativity <liangjiaming@stu.pku.edu.cn>
2024-03-01 18:07:11 +0800

    Adjust example06.

--- 04-Ranges & Algorithms/examples/06-to.cpp
+++ 04-Ranges & Algorithms/examples/06-to.cpp
@@ -9,6 +9,7 @@ namespace stdv = std::views;
 int main()
 {
     std::map<int, double> m{ { 1, 2.0 }, { 3, 4.0 } };
+    // Nope: auto view = m | stdv::values; std::println("{}", view[1]);
     auto valueVec = m | stdv::values | stdr::to<std::vector>();
     std::println("{} ", valueVec[1]);
     return 0;
